1434642473eafc4937de278d33c46437f4370446
duaneo2011 <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2022-09-15 13:12:43 +0200

    initial Update
    initial Update

---
+++ netflix-clone/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="home">Home</div>
+	<div class="featureCard">
+		<router-link to="/movie/tt0409591">
+			<img
+				src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9jdXN8ZW58MHx8MHx8&w=1000&q=80"
+				alt="aleternativeImage"
+				class="featured-img"
+			/>
+			<div class="detail">
+				<h3>image</h3>
+				<p>
+					Must save the turtles. Must save the turtles. Must save the turtles.
+					Must save the turtles. Must save the turtles.
+				</p>
+			</div>
+		</router-link>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {};
+</script>
